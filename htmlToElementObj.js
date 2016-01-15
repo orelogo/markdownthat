3d66a815f26ebdaa75bb94b140bbd8014a5fa51b
orelovo <patso0@gmail.com>
2016-01-15 14:24:54 -0500

    Use arrOpenTag to keep track of currently open tags
      - implement arrOpenTag to hold currently open tags, body is at[0]
      - replace storeElement() with openTag() and closeTag()
      - tested with simple <p> tags: https://www.reddit.com/r/shortstories/comments/3nmmc7/mf_you_cant_skip_stones_on_the_sea/

--- htmlToElementObj.js
+++ htmlToElementObj.js
@@ -8,14 +8,17 @@ function getPostHtml() {
 
 var html = getPostHtml(); // get html from post
 var startCopy, endCopy; // index of where to start and end copying
-var currentDepth = 0; // how deep into
+var currentDepth = 0; // how deep into the element tree the program is
 
-var currentOpenTag; // current opened tag, ie. "p"
-var findOpenTag = /<(p|em|strong|li)[\s\S]*?>/g // regex for finding open tag
+var currentOpenTag = ""; // current opened tag, ie. "p"
+var findOpenTag = /<(p|em|strong|ul|li)[\s\S]*?>/g // regex for finding open tag
 var findCurrentCloseTag; // regex for finding current close tag
 
-var body = new Element(0, "body"); // base containing all elements
-
+// base containing all elements, tag must be "" for proper use in openTag()
+// and closeTag()
+var body = new Element(0, "");
+var arrOpenTags = []; // array of currently open tags
+arrOpenTags.push(body);
 
 function scanHtml() {
 
@@ -25,7 +28,7 @@ function scanHtml() {
 
   while (true) {
     // get next tag based on the previous tag's last index
-    var nextTag = getNextTag(nextTag.lastIndex);
+    nextTag = getNextTag(nextTag.lastIndex);
 
     // end of html when nextTag return false
     if (!nextTag) {
@@ -33,14 +36,10 @@ function scanHtml() {
     }
 
     // an open tag
-    if (nextTag.openTag) {
-      currentOpenTag = nextTag.tag;
-      startCopy = nextTag.lastIndex;
+    if (nextTag.isOpenTag) {
+      openTag(nextTag);
     } else {
-      endCopy = nextTag.matchIndex;
-      currentTextContent = html.slice(startCopy, endCopy);
-      console.log(currentTextContent);
-      storeElement(currentOpenTag, currentTextContent);
+      closeTag(nextTag);
     } // end else
   } // end while loop
 }
@@ -49,7 +48,7 @@ function scanHtml() {
 function getNextTag(lastIndex) {
 
   // set the regex for closing tag based on last tag opened
-  findCurrentCloseTag = new RegExp(("<\/" + currentOpenTag + ">"), "g");
+  findCurrentCloseTag = new RegExp("<\/" + currentOpenTag + ">", "g");
 
   // set regex to search from same last index
   findOpenTag.lastIndex = findCurrentCloseTag.lastIndex = lastIndex;
@@ -57,10 +56,10 @@ function getNextTag(lastIndex) {
   var infoCurrentCloseTag = findCurrentCloseTag.exec(html);
 
   if (!infoOpenTag) {
-    console.log("infoOpenTag is null")
+    console.log("infoOpenTag is null");
   }
   if (!infoCurrentCloseTag) {
-    console.log("infoCurrentCloseTag is null")
+    console.log("infoCurrentCloseTag is null");;
   }
 
   // both regex area null indicating they have reached the end of the html
@@ -68,44 +67,67 @@ function getNextTag(lastIndex) {
     return false;
   }
 
-  // if infoOpenTag is not null and an open tag first
-  if (infoOpenTag && infoOpenTag.index < infoCurrentCloseTag.index) {
-    return new NextTag(true, infoOpenTag[1], infoOpenTag.index,
-        findOpenTag.lastIndex);
+  // if infoOpenTag is not null and an open tag first, or if
+  // findCurrentCloseTag.exec() fails to find a match (ie. when first run
+  // with "")
+  if (!infoCurrentCloseTag ||
+      infoOpenTag && infoOpenTag.index < infoCurrentCloseTag.index) {
+    var nextTag = new NextTag(true, infoOpenTag[1], infoOpenTag.index,
+      findOpenTag.lastIndex);
   } else { // find a close tag first
-    return new NextTag(false, infoCurrentCloseTag[0],
-        infoCurrentCloseTag.index, findCurrentCloseTag.lastIndex);
+    var nextTag = new NextTag(false, infoCurrentCloseTag[0],
+      infoCurrentCloseTag.index, findCurrentCloseTag.lastIndex);
   }
+  return nextTag;
 }
 
+function openTag(nextTag) {
+  // there is a current open tag, add text content to last open element
+  // if you are in body, currentOpenTag = "" and will be false
+  if (currentOpenTag) {
+    endCopy = nextTag.matchIndex;
+    var currentTextContent = html.slice(startCopy, endCopy);
+    arrOpenTags[arrOpenTags.length - 1].content.push(currentTextContent);
+  }
 
-function storeElement(tag, content) {
-  var newElement = new Element(currentDepth + 1, tag);
-  console.log(newElement.content);
-  // add content to new element
-  newElement.content.push(content);
+  // open new tag
+  currentDepth += 1;
+  currentOpenTag = nextTag.tag;
+  startCopy = nextTag.lastIndex;
 
-  // add element to correct depth
-  switch (currentDepth) {
-    case 0:
-      body.content.push(newElement);
-  } // end switch
+  var newElement = new Element(currentDepth, currentOpenTag);
+  // add new element pointer to content of last open tag element
+  arrOpenTags[arrOpenTags.length - 1].content.push(newElement);
+  // add new element pointer to end of arrOpenTags
+  arrOpenTags.push(newElement);
 }
 
+function closeTag(nextTag) {
+  // add text conent to last open element
+  endCopy = nextTag.matchIndex;
+  var currentTextContent = html.slice(startCopy, endCopy);
+  arrOpenTags[arrOpenTags.length - 1].content.push(currentTextContent);
 
-// constrcutor for an element object
+  // remove close tag from end of arrOpenTags
+  arrOpenTags.pop();
+  currentDepth -= 1;
+  // get current open tag for last element in arrOpenTags
+  currentOpenTag = arrOpenTags[arrOpenTags.length - 1].tag;
+}
+
+// construcutor for an element object
 function Element(depth, tag) {
-  this.depth = depth;  // depth of element, 0 is the surface area
-  this.tag = tag;
-  this.content = [];        // content within element
+  this.depth = depth; // depth of element, 0 is the surface area
+  this.tag = tag;     // elment tag
+  this.content = [];  // content within element
 }
 
 // constructor for a next tag object
-function NextTag(openTag, tag, matchIndex, lastIndex) {
-  this.openTag = openTag;   // boolean whether this tag is an open tag
-  this.tag = tag;           // tag, ie. p, /p, em, /em
-  this.matchIndex = matchIndex;       // index of matching tag
-  this.lastIndex = lastIndex;         // index where exec stopped
+function NextTag(isOpenTag, tag, matchIndex, lastIndex) {
+  this.isOpenTag = isOpenTag;   // boolean whether this tag is an open tag
+  this.tag = tag;               // tag, ie. p, /p, em, /em
+  this.matchIndex = matchIndex; // index of matching tag
+  this.lastIndex = lastIndex;   // index where exec stopped
 }
 
 //Tests
